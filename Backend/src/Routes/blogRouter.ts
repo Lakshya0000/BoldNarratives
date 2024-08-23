542aba7c6379e81689a523ec5f2388d9b3153259
Aditya G <aditya.gunjkar04@gmail.com>
2024-08-23 11:50:10 +0530

    added middleware

--- Backend/src/Routes/blogRouter.ts
+++ Backend/src/Routes/blogRouter.ts
@@ -1,8 +1,22 @@
 import { Hono } from "hono";
-
+import authmiddleware from "../middleware";
 export const blogRouter=new Hono<{
     Bindings:{
         DATABASE_URL:string
         
+    },
+    Variables: {
+        userId: string;
     }
 }>()
+
+blogRouter.use('*', authmiddleware)
+
+blogRouter.get('/protected',  (c) => {
+    return c.json({
+        message: 'This is a protected route',
+        userId:  c.get("userId")
+        
+    })
+})
+
